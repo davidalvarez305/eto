12a75268f7e66b9e5992607fc642106f6847062a
david alvarez <david@southfloridaathleticclub.com>
2023-12-09 17:52:57 -0500

    JS

--- django/website/static/blog/main.js
+++ django/website/static/blog/main.js
@@ -1,6 +1,7 @@
 const telephoneButton = document.getElementById('telephone-button');
 
 telephoneButton.addEventListener('click', function() {
-    const phoneNumber = telephoneButton.textContent;
-    window.location.href = 'tel:' + phoneNumber.trim();
+    const phoneNumber = telephoneButton.textContent.trim();
+    window.gtag('event', 'phone_click');
+    window.location.href = 'tel:' + phoneNumber
 });
