793f507b1b707c174e479b527d6e87a590b921ec
david alvarez <david@southfloridaathleticclub.com>
2024-05-11 18:04:32 -0400

    LP BETTER MARKETING ANALYTICS

--- django/website/static/blog/lp.js
+++ django/website/static/blog/lp.js
@@ -4,10 +4,10 @@ const qs = new URLSearchParams(window.location.search);
 let latitude = 0.0;
 let longitude = 0.0;
 
-document.addEventListener('DOMContentLoaded', getUserLocation())
+document.addEventListener("DOMContentLoaded", getUserLocation());
 
 function toTitleCase(str) {
-  return str.replace(/\w\S*/g, function(txt) {
+  return str.replace(/\w\S*/g, function (txt) {
     return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
   });
 }
@@ -19,23 +19,21 @@ function getHostFromURL() {
 
   const parsedURL = new URL(document.referrer);
 
-  const host = parsedURL.hostname.split('.')[1];;
+  const host = parsedURL.hostname.split(".")[1];
 
   return host;
 }
 
 function getLeadChannel() {
-
   // No referrer means the user accessed the website directly
   if (document.referrer.length === 0) return "direct";
 
   // If we get to this point, it means that document.referrer is not empty
   if (qs.size === 0) {
-
     const host = getHostFromURL();
 
-    qs.set('source', host);
-    qs.set('medium', `${toTitleCase(host)} - SEO`);
+    qs.set("source", host);
+    qs.set("medium", `${toTitleCase(host)} - SEO`);
 
     return "organic";
   }
@@ -44,7 +42,7 @@ function getLeadChannel() {
   if (qs.has("gclid") || qs.has("msclkid")) return "paid";
 
   return "other";
-};
+}
 
 function getUserLocation() {
   const options = {
@@ -55,12 +53,12 @@ function getUserLocation() {
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(
-      function(position) {
+      function (position) {
         // Success callback
         latitude = position.coords.latitude;
         longitude = position.coords.longitude;
       },
-      function(error) {
+      function (error) {
         console.error("Error getting user location:", error.message);
       },
       options
@@ -79,31 +77,42 @@ function getUserDeviceInfo() {
 function handleCTAClick(e) {
   const { userAgent, language } = getUserDeviceInfo();
 
-  const buttonName = e.target.getAttribute('name');
+  const buttonName = e.target.getAttribute("name");
   // This set method must be first in order for the getLeadChannel logic to work correctly
   // Because it checks that all qs.entries are of length 0 ('meaning organic traffic')
   // It also checks document.referrer to differentiate direct vs organic
-  qs.set('channel', getLeadChannel());
-  qs.set('referrer', document.referrer);
-  qs.set('landing_page', window.location.href);
-  qs.set('button_clicked', buttonName);
-  qs.set('longitude', longitude);
-  qs.set('latitude', latitude);
-  qs.set('userAgent', userAgent);
-  qs.set('language', language);
+  var data = JSON.parse(localStorage.getItem("user")) || {};
+
+  if (Object.keys(data).length === 0) {
+    data.landingPage = window.location.href;
+    data.referrer = document.referrer;
+    data.trafficSource = getLeadChannel();
+    localStorage.setItem("user", JSON.stringify(data));
+  }
+
+  qs.set("landing_page", data.landingPage);
+  qs.set("referrer", data.referrer);
+  qs.set("channel", data.trafficSource);
+  qs.set("channel", getLeadChannel());
+  qs.set("landing_page", window.location.href);
+  qs.set("button_clicked", buttonName);
+  qs.set("longitude", longitude);
+  qs.set("latitude", latitude);
+  qs.set("userAgent", userAgent);
+  qs.set("language", language);
 
   const currentDomain = new URL(window.location.origin + "/quote");
 
   currentDomain.search = qs.toString();
 
   window.location.replace(currentDomain.href);
-};
+}
 
-quoteButtons.forEach(button => {
+quoteButtons.forEach((button) => {
   let children = button.children;
   console.log(button.name);
-  Array.from(children).forEach(child => {
-      child.setAttribute("name", button.name);
+  Array.from(children).forEach((child) => {
+    child.setAttribute("name", button.name);
   });
 
   button.addEventListener("click", handleCTAClick);
