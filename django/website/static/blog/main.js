7b626e84af94bdf007974fd19fb5c9d19fc0a860
unknown <david@southfloridaathleticclub.com>
2024-07-13 17:03:07 -0400

    login

--- django/website/static/blog/main.js
+++ django/website/static/blog/main.js
@@ -4,12 +4,3 @@ telephoneButton.addEventListener('click', function() {
     const phoneNumber = telephoneButton.textContent.trim();
     window.location.href = 'tel:' + phoneNumber
 });
-
-document.addEventListener("DOMContentLoaded", function() {
-    var landingPage = localStorage.getItem("landingPage");
-    if (!landingPage) {
-        localStorage.setItem("landingPage", window.location.href);
-    } else {
-        document.referrer = landingPage;
-    }
-});
